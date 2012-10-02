26d47890c49d905c9ef7e748334532671a790bff
Brian Jordan <bcjordan@gmail.com>
2012-10-01 21:06:44 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wtfhalloween::Application.config.secret_token = '3ce2235472c98004b94c4e4900865c2733a406931d02e97dfa09fb6e4fc0caa3e14121a67f3ca5047735bf900d6125ec11ed234a5e6c5904e20903e497d96583'
