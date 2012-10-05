56a817ab621875a1d6c3b6fa1f584051b89f89a4
Brian Jordan <bcjordan@gmail.com>
2012-10-04 22:21:37 -0700

    Update tree choice logic and page layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ module ActiveRecord
   class Base
     def self.random
       if (c = count) != 0
-        find(:first, :offset =>rand(c))
+        find(:first, :offset => rand(c))
       end
     end
   end
@@ -16,6 +16,69 @@ end
 
 class HomeController < ApplicationController
   def index
-    @costume = Costume.random
+    session['visitor'] = (!session['visitor'] || params['reset']) ? { } : session['visitor']
+    @visitor           = session['visitor']
+    @visitor.merge!(params)
+
+    # Try a bunch of costumes on, virtually
+    tries = 0
+    while (!@costume || !match(@costume, @visitor))
+      @costume = Costume.random
+      tries    += 1
+      if tries > 350
+        @visitor = { } # Forget everything we've learned. Too specific.
+        break
+      end
+    end
+
+    session['visitor'] = @visitor
+
+    @choice = get_option @visitor, @costume
+  end
+
+  ## TODO: Fix this logic to handle falses
+  def match(costume, visitor)
+    is_match = true
+
+    for key, visitor_choice in visitor
+      if costume.has_attribute? (key)
+        attribute = (costume.read_attribute key)
+      end
+
+      if (visitor_choice == "duh") and (attribute.nil? or attribute == "" or attribute == "FALSE")
+        return false
+      elsif (visitor_choice == "nuh") and not (attribute and attribute != "FALSE")
+        return false
+      end
+    end
+
+    return true
+  end
+
+  # @param [Hash] visitor
+  # @param [Costume] costume
+  def get_option visitor, costume
+    if costume.is_girl and !visitor["girl"]
+      return { phrase: "I'm not a fucking girl", param: "girl=nuh" }
+    elsif costume.is_guy and !visitor["girl"]
+      return { phrase: "I'm not a fucking dude", param: "girl=duh" }
+    elsif costume.is_slutty and !visitor["slutty"]
+      return { phrase: "I'm not a fucking slut", param: "slutty=nuh" }
+    elsif costume.isnt_slutty and !visitor["slutty"]
+      return { phrase: randomize("What is this cutesy shit? I want to get laid", "not slutty enough", "nowhere near slutty enough"), param: "slutty=duh" }
+    else
+      return { phrase: randomize("Forget everything I told you", "You don't know me, reset", "forget what I said"), param: "reset=duh" }
+    end
+  end
+
+  def is s
+    s != nil and s != "nuh"
+  end
+
+  def randomize(*arr)
+    if (arr.length == 0)
+      return 0
+    end
+    return arr[rand(arr.length)]
   end
 end
--- app/models/costume.rb
+++ app/models/costume.rb
@@ -2,4 +2,24 @@ class Costume < ActiveRecord::Base
   attr_accessible :amazon_id, :amazon_link, :buy, :girl, :imgur, :kid, :prime, :quip, :scary, :short_name, :slutty
   validates_uniqueness_of :imgur
   validates_presence_of :amazon_link, :short_name
+
+  def is_girl
+    is girl
+  end
+
+  def is_guy
+    !(is girl)
+  end
+
+  def is_slutty
+    is slutty
+  end
+
+  def isnt_slutty
+    !(is slutty)
+  end
+
+  def is s
+    s != nil and s != "" and s != "FALSE"
+  end
 end
