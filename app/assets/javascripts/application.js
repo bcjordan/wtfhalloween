6258e24051f88ec0c82cd292c5186c46ac0505d1
Brian Jordan <bcjordan@gmail.com>
2012-10-12 09:15:17 -0700

    add google analytics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,16 @@
 //= require bootstrap
 //= require_tree .
 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-35534822-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
 (function (c, a) {
     window.mixpanel = a;
     var b, d, h, e;
